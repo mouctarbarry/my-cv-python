3ad022083a7c0534f258c8e052008c610ab6dc3c
mouctarbarry <mouctar.barry@sogeti.com>
2023-09-01 22:52:09 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from contact import submit_form
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form_route():
+    data = request.form
+
+    errors = {}
+
+    if not data.get('firstname'):
+        errors['firstname'] = "J'aimerais connaître votre prénom."
+    if not data.get('name'):
+        errors['name'] = "Même le nom svp :)"
+    if not data.get('email'):
+        errors['email'] = "Un e-mail pour vous répondre."
+    if not data.get('message'):
+        errors['message'] = "Un petit message pour moi ?"
+
+    if errors:
+        response = {
+            'success': False,
+            'message': errors
+        }
+    else:
+        response = submit_form(data)
+
+    return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
